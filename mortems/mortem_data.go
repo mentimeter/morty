064b1098bab2038c928b58aa0745441294cbc2d2
Oliver Stenbom <oliver@stenbom.eu>
2020-09-23 09:27:42 +0200

    Parse owner, date, time

--- mortems/mortem_data.go
+++ mortems/mortem_data.go
@@ -2,6 +2,7 @@ package mortems
 
 import (
 	"errors"
+	"fmt"
 	"regexp"
 	"time"
 )
@@ -18,6 +19,9 @@ type MortemData struct {
 }
 
 var ErrNoTitle = errors.New("no title of format \"# Title Here\"")
+var ErrNoOwner = errors.New("no owner of format \"Owner: First Last\"")
+var ErrNoDate = errors.New("no date of format \"July 1, 2020\" (no st, nd, th)")
+var ErrNoSeverity = errors.New("no severity of format \"| Severity | sev |\"")
 
 func NewMortemData(fileContent string) {
 
@@ -32,4 +36,42 @@ func ParseTitle(content string) (string, error) {
 	}
 
 	return title[1], nil
+}
+
+func ParseOwner(content string) (string, error) {
+	re := regexp.MustCompile(`.*Owner: (?P<Owner>.+)`)
+
+	owner := re.FindStringSubmatch(content)
+	if owner == nil {
+		return "", ErrNoOwner
+	}
+
+	return owner[1], nil
+}
+
+func ParseDate(content string) (time.Time, error) {
+	re := regexp.MustCompile(`.*Date: (?P<Date>.+)`)
+
+	dateStr := re.FindStringSubmatch(content)
+	if dateStr == nil {
+		return time.Now(), ErrNoDate
+	}
+
+	date, err := time.Parse("January 2, 2006", dateStr[1])
+	if err != nil {
+		return time.Now(), fmt.Errorf("incorrect date format, %s: %w", err, ErrNoDate)
+	}
+
+	return date, nil
+}
+
+func ParseSeverity(content string) (string, error) {
+	re := regexp.MustCompile(` *\| *Severity +\| *(.+) +\| *`)
+
+	sev := re.FindStringSubmatch(content)
+	if sev == nil {
+		return "", ErrNoOwner
+	}
+
+	return sev[1], nil
 }
--- mortems/mortem_data_test.go
+++ mortems/mortem_data_test.go
@@ -2,6 +2,7 @@ package mortems_test
 
 import (
 	"errors"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -20,6 +21,30 @@ var _ = Describe("Mortem Content Parsing", func() {
 			_, err := ParseTitle("It's dead")
 			Expect(err).To(HaveOccurred())
 			Expect(errors.Is(err, ErrNoTitle)).To(BeTrue())
+		})
+	})
+
+	Describe("owner parsing", func() {
+		It("can parse the owner", func() {
+			owner, err := ParseOwner("Owner: Oliver Stenbom")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(owner).To(Equal("Oliver Stenbom"))
+		})
+	})
+
+	Describe("date parsing", func() {
+		It("can parse the date", func() {
+			date, err := ParseDate("Date: June 2, 2020")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(date).To(Equal(time.Date(2020, time.June, 2, 0, 0, 0, 0, time.UTC)))
+		})
+	})
+
+	Describe("severity parsing", func() {
+		It("can parse the severity", func() {
+			sev, err := ParseSeverity("| Severity | 1 |")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(sev).To(Equal("1"))
 		})
 	})
 })
