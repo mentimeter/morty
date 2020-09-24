72dcaf6ef9fc4a8f4ecf1b4f23d0ffd630c9a105
Oliver Stenbom <oliver@stenbom.eu>
2020-09-24 08:32:58 +0200

    Update how-to

--- mortems/how_to.go
+++ mortems/how_to.go
@@ -1,5 +1,7 @@
 package mortems
 
-var howToContent = `# How to create a new post-mortem
-
-`
+const howToContent = "# How to create a new post-mortem\n" +
+	"1. `cp template.md 00x-name-of-your-mortem.md`\n" +
+	"2. Fill out your new post mortem.\n" +
+	"3. Images can go in the `images/` directory if you'd like\n" +
+	"4. Create a pull request and let morty do the rest!\n"
