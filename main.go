275f5b30018bc1fbde04a34e92a913ff0574cf36
Oliver Stenbom <oliver@stenbom.eu>
2021-04-15 16:57:08 +0200

    Can check locally and in git using morty binary

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/mentimeter/morty/mortems"
@@ -12,23 +13,46 @@ import (
 var ErrMissingInput = errors.New("missing required input")
 
 func main() {
-	if err := run(); err != nil {
-		fmt.Fprintf(os.Stderr, "Oh jeez: %s\n", err)
-		os.Exit(1)
+	if len(os.Args) <= 1 {
+		exitHelp()
+	}
+	if os.Args[1] == "check" {
+		if err := runCheck(); err != nil {
+			log.Fatalf("Oh jeez: %s\n", err)
+		}
+	} else if len(os.Args) == 3 && os.Args[1] == "git" && os.Args[2] == "check" {
+		if err := runGitCheck(); err != nil {
+			log.Fatalf("Oh jeez: %s\n", err)
+		}
+	} else if len(os.Args) == 2 && os.Args[1] == "git" {
+		if err := runGitCollect(); err != nil {
+			log.Fatalf("Oh jeez: %s\n", err)
+		}
+	} else {
+		exitHelp()
 	}
 
 	fmt.Println("Post-mortems successfully parsed and organized")
 }
 
-func run() error {
+func exitHelp() {
+	fmt.Println("What do you want me to do? Options are: ")
+	fmt.Println("    `check` - for local parsing")
+	fmt.Println("    `git` - to parse and commit stats")
+	fmt.Println("    `git check` - to check parsing in your github action")
+	log.Fatalln("Please give me an argument!")
+}
+
+func runGitCollect() error {
 	token := githubactions.GetInput("token")
 	if token == "" {
-		return fmt.Errorf("no github token: %w", ErrMissingInput)
+		fmt.Println("Want to check that your post-mortem is correctly formatted? Use `./morty check`")
+		return fmt.Errorf("no github action token supplied: %w", ErrMissingInput)
 	}
 
 	repository := os.Getenv("GITHUB_REPOSITORY")
 	if repository == "" {
-		return fmt.Errorf("no repository: %w", ErrMissingInput)
+		return fmt.Errorf("no repository name set as env variable: %w", ErrMissingInput)
 	}
 
 	ref := os.Getenv("GITHUB_REF")
@@ -37,8 +61,41 @@ func run() error {
 	}
 
 	gitService := mortems.NewGitHubService(token, repository, ref)
-
 	mortemCollector := mortems.NewMortemCollector(gitService)
-
 	return mortemCollector.Collect()
+}
+
+func runGitCheck() error {
+	token := githubactions.GetInput("token")
+	if token == "" {
+		fmt.Println("Want to check that your post-mortem is correctly formatted? Use `./morty check`")
+		return fmt.Errorf("no github action token supplied: %w", ErrMissingInput)
+	}
+
+	repository := os.Getenv("GITHUB_REPOSITORY")
+	if repository == "" {
+		return fmt.Errorf("no repository name set as env variable: %w", ErrMissingInput)
+	}
+
+	ref := os.Getenv("GITHUB_REF")
+	if ref == "" {
+		return fmt.Errorf("no ref: %w", ErrMissingInput)
+	}
+
+	gitService := mortems.NewGitHubService(token, repository, ref)
+	mortemCollector := mortems.NewMortemCollector(gitService)
+	_, err := mortemCollector.Check()
+	return err
+}
+
+func runCheck() error {
+	repoPath, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("could not get wd: %w", err)
+	}
+
+	fileService := mortems.NewLocalFileService(repoPath)
+	mortemCollector := mortems.NewMortemCollector(fileService)
+	_, err = mortemCollector.Check()
+	return err
 }
